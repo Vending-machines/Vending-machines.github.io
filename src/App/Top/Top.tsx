73cbf8d15ae5f88922db9fcdb7342100047fc7c7
sayoi341 <20alt341@gmail.com>
2021-02-15 15:27:08 +0900

    add:トップページのバーを作成

--- src/App/Top/Top.tsx
+++ src/App/Top/Top.tsx
@@ -1,11 +1,12 @@
 import  { FC } from 'react';
 import * as React from "react";
 
-import Col from "../Column/Column";
-
-import "./Top.module.scss";
+import Col from "../Column/Column"
 
 const Top: FC = () => {
+    const columnSetting: string =
+        "column is-10-mobile is-offset-1-mobile is-10-tablet is-offset-1-tablet is-4-desktop is-offset-1-desktop is-4-widescreen is-offset-1-widescreen is-4-fullhd is-offset-1-fullhd";
+
     return(
         <div>
             <section className="hero is-fullheight-with-navbar">
@@ -19,39 +20,66 @@ const Top: FC = () => {
                 </div>
             </section>
 
-            <section className="hero is-white is-fullheight">
-                <div className="hero-body">
-                    <div className="container">
-                        <div className="columns  is-vcentered">
-                            <div data-aos="fade-left" className="column
-                            is-10-mobile is-offset-1-mobile
-                            is-10-tablet is-offset-1-tablet
-                            is-4-desktop is-offset-1-desktop
-                            is-4-widescreen is-offset-1-widescreen
-                            is-4-fullhd is-offset-1-fullhd">
-                                <figure className="image is-square">
-                                    <img src="#" />
-                                </figure>
-                            </div>
-                            <div data-aos="fade-down" className="column
-                            is-10-mobile is-offset-1-mobile
-                            is-10-tablet is-offset-1-tablet
-                            is-5-desktop is-offset-1-desktop
-                            is-5-widescreen is-offset-1-widescreen
-                            is-5-fullhd is-offset-1-fullhd">
-                                <h1 className="titled title is-1 mb-6">
-                                    absurd illustrations that make sense
-                                </h1>
-                                <h2 className="subtitled subtitle">
-                                    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Laborum cupiditate dolorum
-                                    vitae dolores
-                                    nesciunt totam magni quas.
-                                </h2>
+            <div className="BarPoint1">
+                <section id="bar1" className="hero is-small">
+                    <div className="hero-body">
+                        <div className="container">
+                            <div className="columns  is-vcentered">
+                                <div data-aos="fade-left" className={columnSetting}>
+                                    <figure className="image is-square">
+                                        <img src="#" />
+                                    </figure>
+                                </div>
+                                <div data-aos="fade-down" className={columnSetting}>
+                                    <h1 className="titled title is-2 mb-6"><span>売っている</span><span>商品が</span><span>わからない</span></h1>
+                                    <p className="subtitle hide-text is-4">調べたい自販機を選択することで、その自販機で販売されている商品の一覧が表示されます。</p>
+                                </div>
                             </div>
                         </div>
                     </div>
-                </div>
-            </section>
+                </section>
+            </div>
+
+            <div className="BarPoint2">
+                <section id="bar2" className="hero is-small">
+                    <div className="hero-body">
+                        <div className="container">
+                            <div className="columns  is-vcentered hero-2">
+                                <div data-aos="fade-left" className={columnSetting}>
+                                    <h1 className="titled title is-1 mb-6"><span>商品の</span><span>場所が</span><span>わからない</span></h1>
+                                    <p className="subtitle hide-text is-4">調べたい商品を選択することで、商品がどこで売っているか検索できます。</p>
+                                </div>
+                                <div data-aos="fade-down" className={columnSetting}>
+                                    <figure className="image is-square">
+                                        <img src="#" />
+                                    </figure>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </section>
+            </div>
+
+            <div className="BarPoint3">
+                <section id="bar3" className="hero is-small">
+                    <div className="hero-body">
+                        <div className="container">
+                            <div className="columns  is-vcentered">
+                                <div data-aos="fade-left" className={columnSetting}>
+                                    <figure className="image is-square">
+                                        <img src="#" />
+                                    </figure>
+                                </div>
+                                <div data-aos="fade-down" className={columnSetting}>
+                                    <h1 className="titled title is-2 mb-6"><span>どんな</span><span>商品なのか</span><span>わからない</span></h1>
+                                    <p className="subtitle hide-text is-4">商品を検索する際、画像も一緒に確認できます</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </section>
+            </div>
+
         </div>
     );
 };

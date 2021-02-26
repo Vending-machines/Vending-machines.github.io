3ba55357ac59547d83610fef75c00e9da7f11948
sayoi341 <67829283+sayoi341@users.noreply.github.com>
2021-02-26 16:13:50 +0900

    add:pngファイルのimport設定

--- src/global.d.ts
+++ src/global.d.ts
@@ -10,3 +10,6 @@ declare module "*.module.sass" {
     const classes: { readonly [key: string]: string };
     export default classes;
 }
+declare module '*.png'
+declare module '*.jpg'
+declare module '*.gif'

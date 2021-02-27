f50fcf27ed2efa9510236733dea0e2c3ba29377d
sayoi341 <67829283+sayoi341@users.noreply.github.com>
2021-02-27 20:53:42 +0900

    clean:ファイルの整理

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,38 @@
 import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import { render } from "react-dom";
 
-import App from "./App/App";
 
+//module
+import Header from "./components/module/Header/Header";
+import Footer from "./components/module/Footer/Footer";
+
+//pages
+import Top from "./pages/Top/Top";
+import Search from "./pages/Search/Search"
+
+//style
 import "../node_modules/bulma/bulma.sass";
 
+
+
+
+
+
+const pagesName:string[] = ['Top', 'Search', 'Contact'];
 const index = document.getElementById("root");
 
-render(<App />,index);
+function Index() {
+    return (
+        <Router>
+            <Header pages={pagesName} />
+            <main className="mt-6">
+                <Route exact path="/" component={Top} />
+                <Route path="/Search" component={Search} />
+            </main>
+            <Footer />
+        </Router>
+    )
+}
+
+render(<Index />,index);

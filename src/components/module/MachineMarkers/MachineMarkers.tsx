e1635bc4fe0afcd1e7665e42ea4e41dddabbceee
sayoi341 <67829283+sayoi341@users.noreply.github.com>
2021-03-10 02:46:07 +0900

    add: 全自販機の場所指定

--- src/components/module/MachineMarkers/MachineMarkers.tsx
+++ src/components/module/MachineMarkers/MachineMarkers.tsx
@@ -9,6 +9,7 @@ import L from "leaflet";
 import icon from "leaflet/dist/images/marker-icon.png";
 import iconRetina from "leaflet/dist/images/marker-icon-2x.png";
 import iconShadow from "leaflet/dist/images/marker-shadow.png";
+import {futimes} from "fs";
 
 let DefaultIcon = L.icon({
     iconUrl: icon,
@@ -30,20 +31,35 @@ interface LatLngAndBound{
 }
 
 const Marker1:LatLngAndBound = {
-    position:[30, 60],
+    position:[49, 58],
     bound: [[0, 0],[30, 60]]
 }
 const Marker2:LatLngAndBound = {
-    position:[90, 240],
+    position:[40, 178],
     bound:[[90, 240],[90, 240]]
 }
 
+const Marker3: LatLngAndBound = {
+    position: [38, 190],
+    bound: [[0, 0], [0, 0]]
+}
+
+const Marker4: LatLngAndBound = {
+    position: [38, 198],
+    bound: [[0, 0], [0, 0]]
+}
+
+const Marker5: LatLngAndBound = {
+    position: [38, 206],
+    bound: [[0, 0], [0, 0]]
+}
 
 interface Props{
     toggleModal:Function,
+    toggleModalContent:Function
 }
 
-const MachineMarkers:FC<Props> = ({toggleModal}) => {
+const MachineMarkers:FC<Props> = ({toggleModal, toggleModalContent}) => {
 
 
     const map = useMap();
@@ -51,8 +67,8 @@ const MachineMarkers:FC<Props> = ({toggleModal}) => {
     const Handlers1 = useMemo(
         () => ({
             click(){
-                map.fitBounds(Marker1.bound)
                 toggleModal(true)
+                toggleModalContent(1)
             },
         }),
         [map],
@@ -61,7 +77,8 @@ const MachineMarkers:FC<Props> = ({toggleModal}) => {
     const Handlers2 = useMemo(
         () => ({
             click(){
-                map.fitBounds(Marker2.bound)
+                toggleModal(true)
+                toggleModalContent(2)
             },
         }),
         [map],
@@ -84,6 +101,30 @@ const MachineMarkers:FC<Props> = ({toggleModal}) => {
             >
                 <Tooltip direction="auto">
                     uoooooooooo
+                </Tooltip>
+            </Marker>
+
+            <Marker
+                position={Marker3.position}
+            >
+                <Tooltip>
+                    oooo
+                </Tooltip>
+            </Marker>
+
+            <Marker
+                position={Marker4.position}
+            >
+                <Tooltip>
+                    oooo
+                </Tooltip>
+            </Marker>
+
+            <Marker
+                position={Marker5.position}
+            >
+                <Tooltip>
+                    oooo
                 </Tooltip>
             </Marker>
         </>

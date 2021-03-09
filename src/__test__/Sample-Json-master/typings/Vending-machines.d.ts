913c8ee863408e403e35a405b93dd1bb5a23b9a9
sayoi341 <67829283+sayoi341@users.noreply.github.com>
2021-03-10 02:11:41 +0900

    add: テスト用のjsonファイルと定義ファイルを追加

---
+++ src/__test__/Sample-Json-master/typings/Vending-machines.d.ts
@@ -0,0 +1,9 @@
+declare module '../Vending-machines.json' {
+    interface Item_name {
+        [id: string]: string;
+    }
+
+    const data:Item_name;
+
+    export default data;
+}
---
+++ src/__test__/Sample-Json-master/typings/item_name.d.ts
@@ -0,0 +1,9 @@
+declare module '../item_name.json' {
+    interface Item_name {
+        [id: string]: string;
+    }
+
+    const data:Item_name;
+
+    export default data;
+}

09bce92141ec6c32856e43db2fcc9acd02708165
sayoi341 <67829283+sayoi341@users.noreply.github.com>
2021-03-12 23:12:05 +0900

    add: boundの設定
    pos[Lat, Lng]のときbound[[Lat-9, Lng-24],[Lat+9, Lng+24]]

--- src/constants/bounds.tsx
+++ src/constants/bounds.tsx
@@ -7,25 +7,25 @@ interface LatLngAndBound{
 
 export const Marker1:LatLngAndBound = {
     position:[49, 58],
-    bound: [[0, 0],[30, 60]]
+    bound: [[40, 34],[58, 82]]
 }
 
 export const Marker2:LatLngAndBound = {
     position:[40, 178],
-    bound:[[90, 240],[90, 240]]
+    bound:[[31, 154],[49, 202]]
 }
 
 export const Marker3: LatLngAndBound = {
     position: [38, 190],
-    bound: [[0, 0], [0, 0]]
+    bound: [[29, 166], [47, 214]]
 }
 
 export const Marker4: LatLngAndBound = {
     position: [38, 198],
-    bound: [[0, 0], [0, 0]]
+    bound: [[29, 174], [47, 222]]
 }
 
 export const Marker5: LatLngAndBound = {
     position: [38, 206],
-    bound: [[0, 0], [0, 0]]
+    bound: [[29, 182], [47, 230]]
 }
